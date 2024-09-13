b7f287bef8a7116b8e42eddd5ac17ed8480f6bf3
ManhDz20cm <manhnguyenn23077@gmail.com>
2024-09-13 20:09:40 +0700

    Update users

--- resources/views/users/userPersonalInfo.blade.php
+++ resources/views/users/userPersonalInfo.blade.php
@@ -2,181 +2,221 @@
 
 @section('content')
 
-<h1>User Account Main Page</h1>
-<div class="container">
-    <div class="container_sidebar_pri">
-        <button id="personalinfo">Personal Info</button>
-        <button id="account_setting">Account Setting</button>
-        <button id="logout">Log Out</button>
+<div class="container d-flex mx-auto mt-5 rounded">
+    <div class="btn_container w-25">
+        <button class="btn_personal_infor mx-auto mb-3 mt-4" id="btn_personal_infor">Personal Info</button>
+        <button class="btn_account_setting mx-auto mb-3" id="btn_account_setting">Account Setting</button>
+        <button class="btn_logout mx-auto mb-3" id="btn_logout">Log Out</button>
     </div>
-    <div class="container_content_pri">
-        <div class="avt_name_role_pri">
-            <div class="avatar_pri">
-                <img src="">
-            </div>
-            <div class="name_role_pri">
-                <div class="name_pri">John Doe</div>
-                <div class="role_pri">Software Engineering</div>
-            </div>
+    <div class="seperate1 h-100"></div>
+
+    <div class="content_container w-75 d-block">
+        <div class="avatar mt-4 bg-white rounded-circle">
+            <img src="" alt="">
         </div>
-        <div class="phone_email_pri">
-            <div class="phone_pri">Phone Number : 0991277852</div>
-            <div class="email_pri">Email : hoangnguyen23567@gmail.com</div>
+
+        <div class="seperate2 mx-auto mt-4 mb-4"></div>
+        <h3 class="ms-4 mb-3">Personal Information</h3>
+        <div class="name">
+            <span class="infor_field">Username:</span>
+            <span id="output_name">John Doe</span>
+        </div>
+        <div class="role">
+            <span class="infor_field">Role:</span>
+            <span id="output_role">Software Engineering</span>
+            </div>
+
+        <div class="seperate2 mx-auto mt-4 mb-4"></div>
+        <h3 class="ms-4 mb-3">Account Information</h3>
+        <div class="phone">
+            <span class="infor_field">Phone Number:</span>
+            <span id="output_phone">0992133755</span>
+        </div>
+        <div class="email">
+            <span class="infor_field">Email:</span>
+            <span id="output_email">johndoe1998@gmail.com</span>
         </div>
     </div>
 </div>
+<div class="logout_messagebox d-none"></div>
+    <p class="fs-3 text-center text-primary mt-5">Alert !</p>
+    <p class="fs-4 text-center mt-2">Do you want to log out ?</p>
+    <button class="btn_logout_yes btn btn-primary" id="btn_logout_yes">Yes</button>
+    <button class="btn_logout_no btn btn-primary" id="btn_logout_no">No</button>
+</div>
+
 
 @endsection
 
 
 @push('styles')
 <style>
-    h1,h2{
-    text-align: center;
-    font-family: 'Montserrat', sans-serif;
-}
-h2{
-    margin-left: -30px;
+h3{
+    font-family: 'Space Grotesk'; font-size: 25px;
 }
 .container{
-    margin-top: 10px;
-    margin-left: 40px;
-    margin-right: 40px;
-    display: flex;
-    height: 570px;
-    font-family: 'Montserrat', sans-serif;
-}
-.container_sidebar_pri,.container_sidebar_uac{
-    width: 20vw;
-    background-color: white;
-    margin-right: 7px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    text-align: center;
-}
-.container_content_pri{
-    width: 80vw;
-    background-color: white;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    display: block;
-}
-.container_content_uac{
-    width: 80vw;
-    background-color: white;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    display: block;
-}
-.main_content_uac{
-    display: flex;
-}
-.container_change{
-    margin-left: 35px;
-    margin-top: 60px;
-}
-.avt_name_role_pri{
-    display: flex;
-}
-.name_role_pri,.name_role_uac{
-    display: block;
-}
-.phone_email_pri,.phone_email_uac{
-    display: block;
-}
-button{
     width: 80%;
-    height: 50px;
-    border: none;
-    margin: 10px;
-    background-color: white;
-    border: 1px solid black;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-    font-size: 17px;
-}
-#personalinfo{
-    margin-top: 20px;
-}
-.avatar_pri,.avatar_uac{
-    width: 110px;
-    height: 110px;
-    border: 2px solid black;
-    margin: 20px 20px 20px 35px;
-}
-.name_pri,.name_uac{
-    width: 300px;
-    height: 50px;
-    border: 2px solid black;
-    margin-top: 20px;
-    line-height: 50px;
-    align-items: center;
-    font-size: 24px;
-}
-.role_pri,.role_uac{
-    width: 300px;
-    height: 50px;
-    border: 2px solid black;
-    margin-top: 6px;
-    line-height: 50px;
-    align-items: center;
-    font-size: 17px;
-    font-style: italic;
+    height: 560px;
+    /* Windows Border */
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
 }
 
-.phone_pri{
-    width: 400px;
-    height: 50px;
-    border: 2px solid black;
-    margin: 20px 20px 20px 35px;
-    line-height: 50px;
-    align-items: center;
-    font-size: 17px;
-    font-style: italic;
+/* Seperators */
+.seperate1{
+    width: 0.1px;
+    background-color: white;
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
 }
-.phone_uac{
-    width: 400px;
-    height: 50px;
-    border: 2px solid black;
-    margin-top: 20px;
-    margin-left: 20px;
-    line-height: 50px;
-    align-items: center;
-    font-size: 17px;
-    font-style: italic;
+.seperate2{
+    width: 90%;
+    height: 0.1px;
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
 }
-.email_pri{
-    width: 400px;
-    height: 50px;
-    border: 2px solid black;
-    margin: 20px 20px 20px 35px;
-    line-height: 50px;
-    align-items: center;
-    font-size: 17px;
-    font-style: italic;
+
+/* Buttons */
+.btn_personal_infor,.btn_account_setting,.btn_logout{
+    display: block;
+    width: 80%;
+    height: 40px;
+    background-color: white;
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    font-family: 'Space Grotesk'; font-size: 17px;
 }
-.email_uac{
-    width: 400px;
-    height: 50px;
-    border: 2px solid black;
-    margin-top: 6px;
-    margin-left: 20px;
-    line-height: 50px;
-    align-items: center;
-    font-size: 17px;
-    font-style: italic;
-}
-.name_change,.role_change,.phone_change,.email_change{
-    font-family: 'Montserrat', sans-serif;
-    font-size: 17px;
-    width: 393px;
-    height: 50px;
-    border: 2px solid black;
+.changeinfo_confirm,.changeinfo_forgetpass{
+    margin-top: 15px;
     margin-right: 20px;
-    margin-top: 20px;
+    display: inline-block;
+    width: 100px;
+    height: 30px;
+    background-color: white;
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
+    font-family: 'Space Grotesk'; font-size: 17px;
 }
+.changeinfo_forgetpass{
+    width: 200px;
+}
+
+/* Content */
+.uac_old_container_infor{
+    line-height: 1.6;
+}
+.avatar{
+    margin-left: 38px;
+    width: 130px;
+    height: 130px;
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Span */
+span{
+    font-family: 'Space Grotesk';
+    font-size: 20px;
+}
+.name,.role,.phone,.email{
+    width: 80%;
+    height: 40px;
+    margin-left: 35px;
+    background-color: white;
+}
+.infor_field{
+    margin-right: 15px;
+    font-style: italic;
+    font-weight: bold;
+}
+
+
+/* Form, Input */
+form{
+    margin-left: 35px;
+}
+input{
+    display: block;
+    width: 270px;
+    background-color: white;
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
+    border-radius: 30px;
+}
+.changeinfo_spans{
+    margin-right: 15px;
+    line-height: 2.2;
+}
+
+.logout_messagebox{
+    display: none;
+    width: 350px;
+    height: 350px;
+    position: fixed;
+    z-index: 10;
+    top: 25%;
+    left: 37%;
+    background-color: #fff;
+    border: 1px solid #d6d6d6;
+    box-shadow:
+    0 4px 8px rgba(0, 0, 0, 0.1),
+    inset 0 1px 0 rgba(255, 255, 255, 0.3),
+    inset 0 -1px 0 rgba(0, 0, 0, 0.2);
+}
+.btn_logout_yes,.btn_logout_no{
+    width: 100px;
+    margin-left: 47px;
+    margin-top: 30px;
+}
+
 </style>
 @endpush
 
 
 @push(scripts)
 <script>
+addEventListener('DOMContentLoaded',function(){
+    var btn_personal_infor = this.document.getElementById('btn_personal_infor');
+    var btn_account_setting = this.document.getElementById('btn_account_setting');
+    var btn_logout = this.document.getElementById('btn_logout');
+    var logout_messagebox = this.document.getElementById("logout_messagebox");
 
+    var btn_logout_yes = this.document.getElementById('btn_logout_yes');
+    var btn_logout_no = this.document.getElementById('btn_logout_no');
+
+    btn_logout.addEventListener('click',function(){
+        logout_messagebox.style.display = "block";
+        btn_logout_yes.addEventListener('click',function(){
+            alert('Log out succesfully');
+            logout_messagebox.style.display = "none";
+        });
+
+        btn_logout_no.addEventListener('click',function(){
+            logout_messagebox.style.display = "none";
+        });
+    });
+});
 </script>
 @endpush
