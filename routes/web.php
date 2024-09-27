987e11ffda73a18ffe1728dd8e91c38f3c6b3850
DuaDepTrai <hieun.mgc@gmail.com>
2024-09-27 09:51:19 +0700

    Hieu

--- routes/web.php
+++ routes/web.php
@@ -56,22 +56,18 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::put('admin/tours/{id}', [AdminToursController::class, 'update'])->name('admin.tours.update');
     Route::delete('admin/tours/{id}', [AdminToursController::class, 'destroy'])->name('admin.tours.destroy');
 
-Route::get('/admin/order', [AdminOrderController::class, 'index'])->name('admin.order.index');
-Route::get('/admin/order/{id}/edit', [AdminOrderController::class, 'edit'])->name('admin.order.edit');
-Route::put('/admin/order/{id}', [AdminOrderController::class, 'update'])->name('admin.order.update');
-Route::delete('/admin/order/{id}', [AdminOrderController::class, 'destroy'])->name('admin.order.destroy');
+    Route::get('/admin/order', [AdminOrderController::class, 'index'])->name('admin.order.index');
+    Route::get('/admin/order/{id}/edit', [AdminOrderController::class, 'edit'])->name('admin.order.edit');
+    Route::put('/admin/order/{id}', [AdminOrderController::class, 'update'])->name('admin.order.update');
+    Route::delete('/admin/order/{id}', [AdminOrderController::class, 'destroy'])->name('admin.order.destroy');
 
-
-
-Route::get('/admin/UserManagement', [AdminUserManagementController::class, 'index']);
-Route::get('/admin/UserManagement', [AdminUserManagementController::class, 'index'])->name('admin.UserManagement.index');  // Display user list
-Route::get('/admin/UserManagement/create', [AdminUserManagementController::class, 'create'])->name('admin.UserManagement.create');  // Show user addition form
-Route::post('/admin/UserManagement', [AdminUserManagementController::class, 'store'])->name('admin.UserManagement.store');  // Handle new user addition
-Route::get('/admin/UserManagement/{id}/edit', [AdminUserManagementController::class, 'edit'])->name('admin.UserManagement.edit');  // Show user edit form
-Route::put('/admin/UserManagement/{id}', [AdminUserManagementController::class, 'update'])->name('admin.UserManagement.update');  // Handle user information update
-Route::delete('/admin/UserManagement/{id}', [AdminUserManagementController::class, 'destroy'])->name('admin.UserManagement.destroy');  // Handle user deletion
-
-
+    Route::get('/admin/UserManagement', [AdminUserManagementController::class, 'index']);
+    Route::get('/admin/UserManagement', [AdminUserManagementController::class, 'index'])->name('admin.UserManagement.index');  // Display user list
+    Route::get('/admin/UserManagement/create', [AdminUserManagementController::class, 'create'])->name('admin.UserManagement.create');  // Show user addition form
+    Route::post('/admin/UserManagement', [AdminUserManagementController::class, 'store'])->name('admin.UserManagement.store');  // Handle new user addition
+    Route::get('/admin/UserManagement/{id}/edit', [AdminUserManagementController::class, 'edit'])->name('admin.UserManagement.edit');  // Show user edit form
+    Route::put('/admin/UserManagement/{id}', [AdminUserManagementController::class, 'update'])->name('admin.UserManagement.update');  // Handle user information update
+    Route::delete('/admin/UserManagement/{id}', [AdminUserManagementController::class, 'destroy'])->name('admin.UserManagement.destroy');  // Handle user deletion
 
     Route::get('/admin/branches', [AdminBranchesController::class, 'index']);
     Route::get('/admin/branches', [AdminBranchesController::class, 'index'])->name('admin.branches.index');  // Show list branches
@@ -127,8 +123,6 @@ Route::put('/users/{user}',[UsersController::class,'update'])->name('users.updat
 
 Route::get('/personalinfo', [UsersController::class, 'personalInfo'])->name('users.personalinfo');
 Route::get('/settings', [UsersController::class, 'settings'])->name('users.settings');
-
-Route::get('/galleries', [GalleriesController::class, 'index'])->name('galleries.index');
 
 Route::get('/order', [OrderController::class, 'create'])->name('order.create');
 Route::post('/order/confirm', [OrderController::class, 'confirm'])->name('order.confirm');
